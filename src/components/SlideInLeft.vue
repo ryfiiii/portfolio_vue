edd733d4022bc208ae23ac48fb424d38c236a416
ryfiii <mzk114.cube@gmail.com>
2023-10-06 09:35:02 +0900

    アニメーション軽量化

--- src/components/SlideInLeft.vue
+++ src/components/SlideInLeft.vue
@@ -1,48 +1,27 @@
 <template>
-  <div ref="fade" :class="{ slidein: visible, hidden: !visible }">
+  <div ref="slide" :class="{ 'animate__animated animate__fadeInLeft': visible }">
     <slot></slot>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount, watchEffect } from 'vue';
-const fade = ref<HTMLElement | null>(null);
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+
+const slide = ref<HTMLElement | null>(null);
 const visible = ref(false);
+
 const handleScroll = () => {
-  if (fade.value && !visible.value) {
-    const top = fade.value.getBoundingClientRect().top;
+  if (slide.value && !visible.value) {
+    const top = slide.value.getBoundingClientRect().top;
     visible.value = (top + 100) < window.innerHeight;
   }
 };
+
 onMounted(() => {
   window.addEventListener('scroll', handleScroll);
 });
+
 onBeforeUnmount(() => {
   window.removeEventListener('scroll', handleScroll);
 });
-watchEffect(() => {
-  handleScroll();
-});
 </script>
-
-<style scoped>
-.hidden {
-  opacity: 0;
-}
-
-.slidein {
-  animation: slideIn 1.0s;
-  opacity: 1;
-}
-
-@keyframes slideIn {
-  0% {
-    -webkit-transform: translate3d(-200vh, 0, 0);
-    transform: translate3d(-200vh, 0, 0);
-  }
-
-  100% {
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-  }
-}</style>
