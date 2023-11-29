b73893c9e12c691df7b4fbb7f4efe0bdc671e076
ryfiii <mzk114.cube@gmail.com>
2023-11-29 20:45:14 +0900

    syuusei

--- src/components/Production.vue
+++ src/components/Production.vue
@@ -1,114 +1,118 @@
 <template>
-    <FadeIn>
-        <div class="production">
-            <a :href="url" target="_blank" rel="noopener noreferrer">
-                <img :src="image">
-            </a>
-            <div class="text">
-                <h2>
-                    <a :href="url" target="_blank" rel="noopener noreferrer">{{ title }}</a>
-                    <span>
-                        <a :href="github" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
-                    </span>
-                </h2>
-                <p>{{ body }}</p>
-                <p>{{ body2 }}</p>
-                <div class="skill">
-                    <span v-for="skill in skills" :key="skill">{{ skill }}</span>
-                </div>
-            </div>
+  <FadeIn>
+    <div class="production">
+      <a :href="url" target="_blank" rel="noopener noreferrer">
+        <img :src="image">
+      </a>
+      <div class="text">
+        <h2>
+          <a :href="url" target="_blank" rel="noopener noreferrer">{{ title }}</a>
+          <span>
+            <a v-if="github" :href="github" target="_blank" rel="noopener noreferrer"><i
+                class="fa-brands fa-github"></i></a>
+          </span>
+        </h2>
+        <p>{{ body }}</p>
+        <p>{{ body2 }}</p>
+        <div class="skill">
+          <span v-for="skill in skills" :key="skill">{{ skill }}</span>
         </div>
-    </FadeIn>
+      </div>
+    </div>
+  </FadeIn>
 </template>
 
 <script setup lang="ts">
 import FadeIn from "@/components/FadeIn.vue"
 
 defineProps<{
-    image: string,
-    title: string,
-    body: string,
-    body2: string,
-    url: string,
-    github: string,
-    skills: string[],
+  image: string,
+  title: string,
+  body: string,
+  body2: string,
+  url: string,
+  github: string | null,
+  skills: string[],
 }>()
 </script>
 
 <style scoped lang="scss">
 @import "../assets/css/validate-function";
+
 .production {
-    display: flex;
-    width: 1100px;
+  display: flex;
+  width: 1100px;
+  height: 270px;
+  border-radius: 10px;
+  box-shadow: 0px 10px 18px -3px #777777;
+  margin-top: 40px;
+
+  @include mq() {
+    width: 300px;
+    height: auto;
+    flex-direction: column;
+    padding-bottom: 20px;
+  }
+
+  img {
+    display: block;
+    width: 400px;
     height: 270px;
-    border-radius: 10px;
-    box-shadow: 0px 10px 18px -3px #777777;
-    margin-top: 40px;
-    @include mq(){
-        width: 300px;
-        height: auto;
-        flex-direction: column;
-        padding-bottom: 20px;
+    object-fit: cover;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+    border-right: 1px solid #ccc;
+
+    @include mq() {
+      width: 300px;
+      border-top-right-radius: 10px;
+      border-bottom-left-radius: 0;
+    }
+  }
+
+  .text {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin-left: 30px;
+
+    h2 {
+      font-size: 1.7rem;
+      text-align: center;
+      margin: 25px 0;
+
+      i {
+        text-align: center;
+        margin-left: 10px;
+      }
     }
 
-    img {
+    p {
+      font-size: 1.1rem;
+      padding: 0 25px;
+    }
+
+    .skill {
+      display: flex;
+      justify-content: center;
+      margin-top: 20px;
+      flex-wrap: wrap;
+
+      span {
         display: block;
-        width: 400px;
-        height: 270px;
-        object-fit: cover;
-        border-top-left-radius: 10px;
-        border-bottom-left-radius: 10px;
-        border-right: 1px solid #ccc;
-        @include mq(){
-            width: 300px;
-            border-top-right-radius: 10px;
-            border-bottom-left-radius: 0;
-        }
+        background-color: #ccc;
+        margin-right: 10px;
+        padding: 5px 10px;
+        border-radius: 20px;
+        margin-bottom: 10px;
+      }
     }
+  }
 
-    .text {
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        margin-left: 30px;
+  transition: transform 0.4s;
 
-        h2 {
-            font-size: 1.7rem;
-            text-align: center;
-            margin: 25px 0;
-
-            i {
-                text-align: center;
-                margin-left: 10px;
-            }
-        }
-
-        p {
-            font-size: 1.1rem;
-            padding: 0 25px;
-        }
-
-        .skill {
-            display: flex;
-            justify-content: center;
-            margin-top: 20px;
-            flex-wrap: wrap;
-
-            span {
-                display: block;
-                background-color: #ccc;
-                margin-right: 10px;
-                padding: 5px 10px;
-                border-radius: 20px;
-                margin-bottom: 10px;
-            }
-        }
-    }
-
-    transition: transform 0.4s;
-
-    &:hover {
-        transform: scale(1.05);
-    }
+  &:hover {
+    transform: scale(1.05);
+  }
 }
 </style>
--- src/views/Creation.vue
+++ src/views/Creation.vue
@@ -1,113 +1,68 @@
 <template>
-    <section id="creation">
-        <h1>Creation</h1>
-        <p class="details">
-            これまでに学んできた知識を定着させるために制作した作品をいくつか紹介しています。使用した技術スタックやソースコードも公開しておりますので、詳細についてご覧いただけます。お時間が許す際に、ぜひご確認ください。
-        </p>
-        <div class="gallery">
-            <div class="creation-section">
-                <Production 
-                    :image="PortFolio" 
-                    :title="'PortFolio'" 
-                    :body="'私が使用できる、技術・技術レベル・制作物 等を紹介するために作成しました。'" 
-                    :body2="'制作物のソースコードはGithubのアイコンをクリックすることで見ることができます。'"
-                    :url="'https://ryfiii-portfolio.vercel.app/'"
-                    :github="'https://github.com/ryfiiii/portfolio_vue'"
-                    :skills="['Vue.js', 'TypeScript', 'Sass']">
-                </Production>
-                <Production 
-                    :image="PicShare" 
-                    :title="'PicShare'" 
-                    :body="'ログイン機能、文章・画像・タグ投稿機能、タグ検索機能、投稿編集・削除機能を実装した簡易版Instagram。読みやすいコードを意識しながらコーディングし、デプロイまで行いました。'" 
-                    :body2="'sample@sample.com / sample123 でログインできます'"
-                    :url="'http://ryfiii.ddo.jp/'"
-                    :github="'https://github.com/ryfiiii/PicShare'"
-                    :skills="['Laravel', 'BootStrap', 'AWS', 'Linux']">
-                </Production>
-                <Production 
-                    :image="Sample_inc" 
-                    :title="'サンプル株式会社ウェブサイト'" 
-                    :body="'Vue.js3の練習用に作成しました。'" 
-                    :body2="'ロードアニメーションや切り替えアニメーションを実装しています。'"
-                    :url="'https://vue-sampleinc-website.netlify.app/'"
-                    :github="'https://github.com/ryfiiii/vue_samplesite'"
-                    :skills="['Vue.js', 'Sass']">
-                </Production>
-                <Production 
-                    :image="ExpressVueChatapp" 
-                    :title="'RealtimeChat(express)'" 
-                    :body="'FrontendとBackendが完全に独立したチャットアプリ'" 
-                    :body2="'ファイルを含むデータの受け渡しはjson形式で行っており、チャットイベントの検知はsocket.ioを使用しています。'"
-                    :url="'https://github.com/ryfiiii/vue_express_chatapp'"
-                    :github="'https://github.com/ryfiiii/vue_express_chatapp'"
-                    :skills="['Express', 'Vue.js', 'TypeScript', 'socket.io', 'MySQL', 'Prisma']">
-                </Production>
-                <Production 
-                    :image="Laravel_Stripe_Shop" 
-                    :title="'StripeShop'" 
-                    :body="'購入したブログのみを閲覧できる、ブログショッピングアプリです。'" 
-                    :body2="'決済システムにはStripe、ブログにはMicroCMSを使用しています。'"
-                    :url="'https://github.com/ryfiiii/Laravel_Stripe_Blog/'"
-                    :github="'https://github.com/ryfiiii/Laravel_Stripe_Blog'"
-                    :skills="['Laravel', 'Stripe', 'MicroCMS', 'TailwindCss', 'MySQL']">
-                </Production>
-                <Production 
-                    :image="BlogSite" 
-                    :title="'ブログサイト'" 
-                    :body="'Vue.jsとMicroCMSを組み合わせて、ブログサイトを作成しました。'" 
-                    :body2="'Axiosを使用して、MicroCMSで作成したブログを取得しています。'"
-                    :url="'https://ryfiii-blog.netlify.app/'"
-                    :github="'https://github.com/ryfiiii/blog'"
-                    :skills="['Vue.js', 'Sass', 'MicroCMS']">
-                </Production>
-                <Production 
-                    :image="RealtimeChat" 
-                    :title="'RealtimeChat(Laravel)'"
-                    :body="'LINEのようにリアルタイムでやり取りができるチャットアプリ'"
-                    :body2="'PusherとLaravel Echoを活用して、ユーザー間のリアルタイムな対話を可能にし、且つデータベースに情報を保存し永続化を実現しています。'"
-                    :url="'https://github.com/ryfiiii/ChatApp_VuexLaravel'"
-                    :github="'https://github.com/ryfiiii/ChatApp_VuexLaravel'"
-                    :skills="['Vue.js', 'Laravel', 'Pusher', 'Laravel Echo', 'Sass', 'MySQL']">
-                </Production>
-                <Production 
-                    :image="ExpressTsRestapi" 
-                    :title="'express-ts-restapi'" 
-                    :body="'Express, Typescript, Prismaを使用して作成した簡単なRestfulApi'" 
-                    :body2="'GET/POST/PUT/DELETE それぞれのリクエストに対応しています。'"
-                    :url="'https://github.com/ryfiiii/express_ts_restapi/'"
-                    :github="'https://github.com/ryfiiii/express_ts_restapi'"
-                    :skills="['Node.js', 'Express', 'TypeScript', 'Prisma']">
-                </Production>
-                <Production 
-                    :image="WeatherApp" 
-                    :title="'WeatherApp'"
-                    :body="'OpenWeatherApiから天気を取得し、対応した背景に更新するアプリ'" 
-                    :body2="'　'"
-                    :url="'https://ryfiii-weatherapp.netlify.app/'"
-                    :github="'https://github.com/ryfiiii/weatherapp'"
-                    :skills="['JavaScript', 'HTML', 'CSS']">
-                </Production>
-                <!-- <Production 
-                    :image="RestApi" 
-                    :title="'RestApi'"
-                    :body="'RestfulApiのサンプル。GET/POST/PUT/DELETEそれぞれのリクエストを送り、結果を取得できます。'" 
-                    :body2="'ApiKeyの認証はLaravelのミドルウェアで行っています。'"
-                    :url="'https://ryfiii-404-notfound.vercel.app/'"
-                    :github="'https://github.com/ryfiiii/RestfulApi-sample'"
-                    :skills="['PHP', 'Laravel', 'MySQL']">
-                </Production> -->
-                <!-- <Production 
-                    :image="VueLaravelTodo" 
-                    :title="'Vue × Laravel TodoApp'"
-                    :body="'Laravelを用いてDBとの通信を行うAPIを作成しました。'" 
-                    :body2="'フロントにVue.jsを使用し、axiosでAPIを呼び出してデータを取得し、表示内容を動的に更新しています。'"
-                    :url="'https://ryfiii-404-notfound.vercel.app/'"
-                    :github="'https://github.com/ryfiiii/Todo_Laravel'"
-                    :skills="['PHP', 'Laravel', 'MySQL', 'Vue.js', 'BootStrap']">
-                </Production> -->
-            </div>
-        </div>
-    </section>
+  <section id="creation">
+    <h1>Creation</h1>
+    <p class="details">
+      これまでに学んできた知識を定着させるために制作した作品をいくつか紹介しています。使用した技術スタックやソースコードも公開しておりますので、詳細についてご覧いただけます。お時間が許す際に、ぜひご確認ください。
+    </p>
+    <div class="gallery">
+      <div class="creation-section">
+        <Production :image="PortFolio" :title="'PortFolio'" :body="'私が使用できる、技術・技術レベル・制作物 等を紹介するために作成しました。'"
+          :body2="'制作物のソースコードはGithubのアイコンをクリックすることで見ることができます。'" :url="'https://ryfiii-portfolio.vercel.app/'"
+          :github="null" :skills="['Vue.js', 'TypeScript', 'Sass']">
+        </Production>
+        <Production :image="PicShare" :title="'PicShare'"
+          :body="'ログイン機能、文章・画像・タグ投稿機能、タグ検索機能、投稿編集・削除機能を実装した簡易版Instagram。読みやすいコードを意識しながらコーディングし、デプロイまで行いました。'" :body2="''"
+          :url="'https://github.com/ryfiiii/PicShare'" :github="'https://github.com/ryfiiii/PicShare'"
+          :skills="['Laravel', 'BootStrap', 'AWS', 'Linux']">
+        </Production>
+        <Production :image="ExpressVueChatapp" :title="'RealTimeChat(express)'" :body="'FrontendとBackendが完全に独立したチャットアプリ'"
+          :body2="'ファイルを含むデータの受け渡しはjson形式で行っており、チャットイベントの検知はsocket.ioを使用しています。'"
+          :url="'https://github.com/ryfiiii/vue_express_chatapp'"
+          :github="'https://github.com/ryfiiii/vue_express_chatapp'"
+          :skills="['Express', 'Vue.js', 'TypeScript', 'socket.io', 'MySQL', 'Prisma']">
+        </Production>
+        <Production :image="Sample_inc" :title="'サンプル株式会社ウェブサイト'" :body="'Vue.js3の練習用に作成しました。'"
+          :body2="'ロードアニメーションや切り替えアニメーションを実装しています。'" :url="'https://vue-sampleinc-website.netlify.app/'"
+          :github="'https://github.com/ryfiiii/vue_samplesite'" :skills="['Vue.js', 'Sass']">
+        </Production>
+        <Production :image="Laravel_Stripe_Shop" :title="'StripeShop'" :body="'購入したブログのみを閲覧できる、ブログショッピングアプリです。'"
+          :body2="'決済システムにはStripe、ブログにはMicroCMSを使用しています。'" :url="'https://github.com/ryfiiii/Laravel_Stripe_Blog/'"
+          :github="'https://github.com/ryfiiii/Laravel_Stripe_Blog'"
+          :skills="['Laravel', 'Stripe', 'MicroCMS', 'TailwindCss', 'MySQL']">
+        </Production>
+        <Production :image="BlogSite" :title="'ブログサイト'" :body="'Vue.jsとMicroCMSを組み合わせて、ブログサイトを作成しました。'"
+          :body2="'Axiosを使用して、MicroCMSで作成したブログを取得しています。'" :url="'https://ryfiii-blog.netlify.app/'"
+          :github="'https://github.com/ryfiiii/blog'" :skills="['Vue.js', 'Sass', 'MicroCMS']">
+        </Production>
+        <Production :image="RealtimeChat" :title="'RealtimeChat(Laravel)'" :body="'LINEのようにリアルタイムでやり取りができるチャットアプリ'"
+          :body2="'PusherとLaravel Echoを活用して、ユーザー間のリアルタイムな対話を可能にし、且つデータベースに情報を保存し永続化を実現しています。'"
+          :url="'https://github.com/ryfiiii/ChatApp_VuexLaravel'"
+          :github="'https://github.com/ryfiiii/ChatApp_VuexLaravel'"
+          :skills="['Vue.js', 'Laravel', 'Pusher', 'Laravel Echo', 'Sass', 'MySQL']">
+        </Production>
+        <Production :image="ExpressTsRestapi" :title="'express-ts-restapi'"
+          :body="'Express, Typescript, Prismaを使用して作成した簡単なRestfulApi'" :body2="'GET/POST/PUT/DELETE それぞれのリクエストに対応しています。'"
+          :url="'https://github.com/ryfiiii/express_ts_restapi/'"
+          :github="'https://github.com/ryfiiii/express_ts_restapi'"
+          :skills="['Node.js', 'Express', 'TypeScript', 'Prisma']">
+        </Production>
+        <Production :image="WeatherApp" :title="'WeatherApp'" :body="'OpenWeatherApiから天気を取得し、対応した背景に更新するアプリ'" :body2="'　'"
+          :url="'https://ryfiii-weatherapp.netlify.app/'" :github="'https://github.com/ryfiiii/weatherapp'"
+          :skills="['JavaScript', 'HTML', 'CSS']">
+        </Production>
+        <!-- <Production :image="RestApi" :title="'RestApi'"
+          :body="'RestfulApiのサンプル。GET/POST/PUT/DELETEそれぞれのリクエストを送り、結果を取得できます。'"
+          :body2="'ApiKeyの認証はLaravelのミドルウェアで行っています。'" :url="'https://github.com/ryfiiii/RestfulApi-sample'"
+          :github="'https://github.com/ryfiiii/RestfulApi-sample'" :skills="['PHP', 'Laravel', 'MySQL']">
+        </Production> -->
+        <!-- <Production :image="VueLaravelTodo" :title="'Vue × Laravel TodoApp'" :body="'Laravelを用いてDBとの通信を行うAPIを作成しました。'"
+          :body2="'フロントにVue.jsを使用し、axiosでAPIを呼び出してデータを取得し、表示内容を動的に更新しています。'"
+          :url="'https://github.com/ryfiiii/Todo_Laravel/'" :github="'https://github.com/ryfiiii/Todo_Laravel'"
+          :skills="['PHP', 'Laravel', 'MySQL', 'Vue.js', 'BootStrap']">
+        </Production> -->
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -129,28 +84,28 @@ import ExpressVueChatapp from "@/assets/images/creation/express_vue_chatapp.png"
 
 <style scoped lang="scss">
 #creation {
-    padding: 50px 0 100px 0;
-    background-color: white;
+  padding: 50px 0 100px 0;
+  background-color: white;
 
-    .details {
-        width: 80%;
-        margin: 30px auto;
-        font-size: 1.1rem;
-    }
+  .details {
+    width: 80%;
+    margin: 30px auto;
+    font-size: 1.1rem;
+  }
 
-    h1 {
-        text-align: center;
-        font-size: 1.8rem;
-        text-shadow: 2px 3px 3px rgba(0, 0, 0, 0.4);
-    }
+  h1 {
+    text-align: center;
+    font-size: 1.8rem;
+    text-shadow: 2px 3px 3px rgba(0, 0, 0, 0.4);
+  }
 
-    .gallery {
-        .creation-section {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-        }
+  .gallery {
+    .creation-section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
     }
+  }
 }
 </style>
